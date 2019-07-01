9a3c79cf903650987ad0729c9a3ba736aee5aacf
Buvaneshwari-m <52272256+Buvaneshwari-m@users.noreply.github.com>
2019-07-01 11:27:24 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,5 +1,5 @@
 j = input(" ")
 if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
-    print(j, "is an Alphabet")
+    print (" Alphabet")
 else:
-    print(j, "is not an Alphabet")
+    print( "No")
